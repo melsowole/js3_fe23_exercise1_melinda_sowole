96f6bbb6a4de0d62e01c69ca762caeb50a98eb1b
melsowole <82158719+melsowole@users.noreply.github.com>
2024-09-18 10:15:48 +0200

    stuffs

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,15 +12,38 @@ for (const item of navItems) {
 
 async function navigate(pageName) {
   // define path
-  const path = "./views/" + pageName + ".html";
+  const path = "./" + pageName + ".txt";
+
+  //start load
+  loading();
 
   // change url
-  history.pushState(null, "", "/" + pageName);
+  history.pushState(null, "", "/" + pageName + ".html");
 
   // fetch page
   const response = await fetch(path);
   const page = await response.text();
 
+  // resolve load
+  notLoading();
+
   // set page on screen
   document.querySelector("main").innerHTML = page;
+}
+
+function loading() {
+  const loadWrapper = document.createElement("div");
+  loadWrapper.id = "loadWrapper";
+
+  const load = document.createElement("div");
+  load.textContent = "loading...";
+  loadWrapper.append(load);
+
+  document.body.append(loadWrapper);
+}
+
+function notLoading() {
+  const loadWrapper = document.querySelector("#loadWrapper");
+
+  loadWrapper.remove();
 }
